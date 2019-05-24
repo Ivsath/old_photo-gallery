965e2fb642334fdf75dcc4cfade8dccfd17d74c0
Irvin Reyes <reyes.yerp@gmail.com>
2019-05-24 02:20:11 -0500

    Add a custom 404 page

--- main.go
+++ main.go
@@ -16,8 +16,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@something.com\">support@something.com</a>")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry, but we couldn't find the page you were looking for</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3001", r)
